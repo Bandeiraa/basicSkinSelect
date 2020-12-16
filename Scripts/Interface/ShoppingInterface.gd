c4391a44631e916e2ac8cacf3a436c11a2df9d79
Davi <fcodavibandeira@edu.unifor.br>
2020-12-16 15:27:26 -0300

    Adding a basic animation

--- Scripts/Interface/ShoppingInterface.gd
+++ Scripts/Interface/ShoppingInterface.gd
@@ -1,9 +1,10 @@
 extends Control
 
 onready var animator = get_node("ArrowAnimator")
-onready var currentSprite = get_node("ShoppingBackground/CurrentCharacterSprite")
-onready var previousSprite = get_node("ShoppingBackground/PreviousCharacterSprite")
-onready var nextSprite = get_node("ShoppingBackground/NextCharacterSprite")
+onready var currentSprite = get_node("ShoppingBackground/CurrentRealSprite")
+onready var previousSprite = get_node("ShoppingBackground/PreviousRealSprite")
+onready var nextSprite = get_node("ShoppingBackground/NextRealSprite")
+onready var swipeAnimator = get_node("SwipeAnimator")
 
 var arrowCount = 0
 
@@ -22,24 +23,23 @@ func onLeftArrowPressed():
 	if arrowCount == 0:
 		$ShoppingBackground/LeftArrowButton.set_disabled(true)
 	else:
+		swipeAnimator.play("leftSwipe")
 		nextSprite.set_visible(true)
 		arrowCount -= 1
-		currentSprite.texture = load("res://Sprites/Character/character" + str(arrowCount) + ".png")
 		$ShoppingBackground/RightArrowButton.set_disabled(false)
-		if arrowCount >= 1:
-			previousSprite.texture = load("res://Sprites/Character/character" + str(arrowCount - 1) + ".png")
-		if arrowCount <= 4:
-			nextSprite.texture = load("res://Sprites/Character/character" + str(arrowCount + 1) + ".png")
-
+		
 func onRightArrowPressed():
 	if arrowCount == 5:
 		$ShoppingBackground/RightArrowButton.set_disabled(true)
 	else:
+		swipeAnimator.play("rightSwipe")
 		previousSprite.set_visible(true)
 		arrowCount += 1
-		currentSprite.texture = load("res://Sprites/Character/character" + str(arrowCount) + ".png")
 		$ShoppingBackground/LeftArrowButton.set_disabled(false)
-		if arrowCount >= 1:
+		
+func canSwap():
+	currentSprite.texture = load("res://Sprites/Character/character" + str(arrowCount) + ".png")
+	if arrowCount >= 1:
 			previousSprite.texture = load("res://Sprites/Character/character" + str(arrowCount - 1) + ".png")
-		if arrowCount <= 4:
-			nextSprite.texture = load("res://Sprites/Character/character" + str(arrowCount + 1) + ".png")
+	if arrowCount <= 4:
+		nextSprite.texture = load("res://Sprites/Character/character" + str(arrowCount + 1) + ".png")
